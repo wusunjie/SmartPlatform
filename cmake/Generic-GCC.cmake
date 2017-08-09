3b901dd185cb13a2fb4be1b1b6239b5d2d1c6c74
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-08-09 11:04:09 +0800

    add assembly source properly. define macro in project properly.

--- cmake/Generic-GCC.cmake
+++ cmake/Generic-GCC.cmake
@@ -1,17 +1,18 @@
-if(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m4")
+IF(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m4")
     SET(ARCH_FLAGS "${CMAKE_C_FLAGS} -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+    SET_PROPERTY(SOURCE ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM4.S PROPERTY LANGUAGE C)
     LIST(APPEND SRC_LIST ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM4.S)
-elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m3")
+ELSEIF(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m3")
     SET(ARCH_FLAGS "${CMAKE_C_FLAGS} -mcpu=cortex-m3 -march=armv7-m -mthumb -msoft-float")
     LIST(APPEND SRC_LIST ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM3.S)
-elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-a9")
+ELSEIF(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-a9")
     SET(ARCH_FLAGS "${CMAKE_C_FLAGS} -mcpu=cortex-a9 -march=armv7-a -mthumb -mfloat-abi=hard -mfpu=neon")
-else()
+ELSE()
     message(FATAL_ERROR
         "No C compiler for '${CMAKE_SYSTEM_PROCESSOR}'"
         " is supported yet."
     )
-endif()
+ENDIF()
 
 SET(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -T${LINK_SCRIPT}" CACHE INTERNAL "")
 SET(CMAKE_C_FLAGS "${ARCH_FLAGS} ${STARTUP_DEFS} -Os -flto -ffunction-sections -fdata-sections")
