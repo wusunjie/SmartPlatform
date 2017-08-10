0f541824491377dc5ced09f09d10dc641843a007
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-08-10 13:20:57 +0800

    modify for cmake file.

--- cmake/core.cmake
+++ cmake/core.cmake
@@ -1,11 +1,8 @@
 SET(STM32_PLATFORM_TOP ${CMAKE_CURRENT_LIST_DIR}/..)
 
-IF(CMAKE_HOST_UNIX)
-    SET(CMAKE_TOOLCHAIN_FILE ${STM32_PLATFORM_TOP}/cmake/toolchain-arm-none-eabi.cmake)
-ENDIF()
-
 INCLUDE(${STM32_PLATFORM_TOP}/cmake/board-config-${STM32_PLATFORM_BOARD_NAME}.cmake)
 
 IF(CMAKE_HOST_UNIX)
+    SET(CMAKE_TOOLCHAIN_FILE ${STM32_PLATFORM_TOP}/cmake/toolchain-arm-none-eabi.cmake)
     INCLUDE(${STM32_PLATFORM_TOP}/cmake/generic-gcc.cmake)
 ENDIF()
