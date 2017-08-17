f8e2323e7b6d5329eb56ffe1107bdb74fb2b75e5
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-08-18 00:21:01 +0800

    use startup in STM32 HAL packet.

--- cmake/generic-gcc.cmake
+++ cmake/generic-gcc.cmake
@@ -1,15 +1,9 @@
 IF(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m4")
     SET(ARCH_FLAGS "${CMAKE_C_FLAGS} -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16")
-    SET_PROPERTY(SOURCE ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM4.S PROPERTY LANGUAGE C)
-    LIST(APPEND SRC_LIST ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM4.S)
 ELSEIF(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m3")
     SET(ARCH_FLAGS "${CMAKE_C_FLAGS} -mcpu=cortex-m3 -march=armv7-m -mthumb -msoft-float")
-    SET_PROPERTY(SOURCE ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM3.S PROPERTY LANGUAGE C)
-    LIST(APPEND SRC_LIST ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM3.S)
 ELSEIF(CMAKE_SYSTEM_PROCESSOR STREQUAL "cortex-m7")
     SET(ARCH_FLAGS "${CMAKE_C_FLAGS} -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16")
-    SET_PROPERTY(SOURCE ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM7.S PROPERTY LANGUAGE C)
-    LIST(APPEND SRC_LIST ${STM32_PLATFORM_TOP}/arch/startup/startup_ARMCM7.S)
 ELSE()
     message(FATAL_ERROR
         "No C compiler for '${CMAKE_SYSTEM_PROCESSOR}'"
@@ -22,6 +16,9 @@ IF(STM32_PLATFORM_USE_FREERTOS STREQUAL "true")
 ELSE()
     SET(LINK_SCRIPT ${STM32_PLATFORM_TOP}/board/${STM32_PLATFORM_BOARD_NAME}/Script/${STM32_PLATFORM_BOARD_NAME}-nosys.ld)
 ENDIF()
+
+SET_PROPERTY(SOURCE ${STM32_PLATFORM_TOP}/board/CMSIS/Device/Source/startup_${STM32_PLATFORM_MCU_TYPE}.s PROPERTY LANGUAGE C)
+LIST(APPEND SRC_LIST ${STM32_PLATFORM_TOP}/board/CMSIS/Device/Source/startup_${STM32_PLATFORM_MCU_TYPE}.s)
 
 SET(CMAKE_EXE_LINKER_FLAGS "-L${STM32_PLATFORM_TOP} -Wl,--gc-sections --specs=nosys.specs -T${LINK_SCRIPT}" CACHE INTERNAL "" FORCE)
 SET(CMAKE_C_FLAGS "${ARCH_FLAGS} ${STARTUP_DEFS} -Os -flto -ffunction-sections -fdata-sections" CACHE INTERNAL "" FORCE)
