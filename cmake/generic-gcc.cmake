0bec77181e6402be6f71fecaa547b5c7fd4420ed
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-08-24 20:49:00 +0800

    modify for project.

--- cmake/generic-gcc.cmake
+++ cmake/generic-gcc.cmake
@@ -29,14 +29,16 @@ ELSE()
     )
 ENDIF()
 
+IF(STM32_PLATFORM_BUILD_APP STREQUAL "false")
+    INCLUDE_DIRECTORIES(${STM32_PLATFORM_TOP}/board/common/Include)
+    AUX_SOURCE_DIRECTORY(${STM32_PLATFORM_TOP}/board/common/Source SRC_LIST)
+ENDIF()
+
 INCLUDE(${STM32_PLATFORM_TOP}/cmake/rtos-freertos.cmake)
 SET(LINK_SCRIPT ${STM32_PLATFORM_TOP}/board/${STM32_PLATFORM_BOARD_NAME}/Script/${STM32_PLATFORM_BOARD_NAME}-freertos.ld)
 
 SET(CMAKE_EXE_LINKER_FLAGS "-L${STM32_PLATFORM_TOP} -Wl,--gc-sections -Wl,-Map,${STM32_PLATFORM_BOARD_NAME}-Platform.map --specs=nosys.specs -T${LINK_SCRIPT}" CACHE INTERNAL "" FORCE)
 SET(CMAKE_C_FLAGS "${ARCH_FLAGS} ${STARTUP_DEFS} -g -Os -flto -ffunction-sections -fdata-sections" CACHE INTERNAL "" FORCE)
-
-INCLUDE_DIRECTORIES(${STM32_PLATFORM_TOP}/board/common/Include)
-AUX_SOURCE_DIRECTORY(${STM32_PLATFORM_TOP}/board/common/Source SRC_LIST)
 
 # post-process elf files into .hex files:
 FUNCTION(CREATE_IMAGE target_name)
