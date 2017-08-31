9d5320c8779013e8519c045bbbd7217133d4d156
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-08-31 22:53:20 +0800

    read/write can only used in task.

--- platform/Include/FreeRTOSConfig.h
+++ platform/Include/FreeRTOSConfig.h
@@ -88,7 +88,7 @@ extern uint32_t SystemCoreClock;
 #define configUSE_PREEMPTION					1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
 #define configUSE_QUEUE_SETS					1
-#define configUSE_IDLE_HOOK						1
+#define configUSE_IDLE_HOOK						0
 #define configUSE_TICK_HOOK						1
 #define configCPU_CLOCK_HZ						( SystemCoreClock )
 #define configTICK_RATE_HZ						( 1000 )
--- platform/Source/main.c
+++ platform/Source/main.c
@@ -7,7 +7,10 @@
 
 #include <unistd.h>
 
+static char buf[7] = {0};
+
 static void AppTimerCallback( TimerHandle_t xTimer );
+static void PlatformSetupTask( void *args );
 
 static void AppTimerCallback( TimerHandle_t xTimer )
 {
@@ -21,6 +24,8 @@ static void AppTimerCallback( TimerHandle_t xTimer )
         val = !val;
         write(DEV_LEDGPIO4_ID, &val, 1);
     }
+
+    write(DEV_GPSCOM_ID, buf, 6);
 }
 
 static TimerHandle_t timer;
@@ -29,12 +34,22 @@ int main(void)
 {
     static StaticTimer_t BlinkTimer;
 
+    /* Structure that will hold the TCB of the task being created. */
+    static StaticTask_t xTaskBuffer;
+
+    /* Buffer that the task being created will use as its stack.  Note this is
+    an array of StackType_t variables.  The size of StackType_t is dependent on
+    the RTOS port. */
+    static StackType_t xStack[ 200 ];
+
     char val = 1;
 
     system_init();
 
     write(DEV_LEDGPIO1_ID, &val, 1);
     write(DEV_LEDGPIO2_ID, &val, 1);
+
+    xTaskCreateStatic(PlatformSetupTask, "PlatformSetup", 200, NULL, 1, xStack, &xTaskBuffer);
 
     timer = xTimerCreateStatic("BlinkTimer", 1000 / portTICK_PERIOD_MS, pdTRUE, NULL, AppTimerCallback, &BlinkTimer);
 
@@ -68,12 +83,16 @@ void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackTy
     *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
 
-void vApplicationIdleHook( void )
+void PlatformSetupTask( void *args )
 {
-    if (pdFALSE == xTimerIsTimerActive(timer)) {
-        int val = 1;
-        write(DEV_LEDGPIO3_ID, &val, 1);
-        write(DEV_LEDGPIO4_ID, &val, 1);
-        xTimerStart(timer, 0);
-    }
+    int val = 1;
+
+    read(DEV_GPSCOM_ID, buf, 6);
+
+    write(DEV_LEDGPIO3_ID, &val, 1);
+    write(DEV_LEDGPIO4_ID, &val, 1);
+
+    xTimerStart(timer, 0);
+
+    vTaskSuspend(NULL);
 }
