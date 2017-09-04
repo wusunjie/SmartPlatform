54390d4b6ecb6376c14c7d806cba9c0437b884fa
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-09-04 18:49:33 +0800

    remove core.cmake, it only contains few lines.

--- cmake/core.cmake
+++
@@ -1,4 +0,0 @@
-INCLUDE(${STM32_PLATFORM_TOP}/cmake/Boards/${STM32_PLATFORM_BOARD_NAME}.cmake)
-SET(CMAKE_TOOLCHAIN_FILE ${STM32_PLATFORM_TOP}/cmake/toolchain-arm-none-eabi.cmake)
-
-INCLUDE(${STM32_PLATFORM_TOP}/cmake/generic-gcc.cmake)
