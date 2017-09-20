a94d8f678f001f112d6dfcb1189d75e9feba6e0c
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-09-20 10:27:20 +0800

    install path dependent on the environment variable.

--- README.md
+++ README.md
@@ -7,7 +7,21 @@
 
 ## build
 
-make BOARD_NAME=waveshare
+###Linux:
+
+1. export ARM_NONE_EABI_HOME=/usr.
+
+2. make BOARD_NAME=waveshare.
+
+###Windows:
+
+1. Set environment variable ARM_NONE_EABI_HOME=<gcc-arm-none-eabi install path>.
+
+For example: C:/GNU_Tools_ARM_Embedded/6_2017_q2_update
+
+2. Use CMake GUI, set the source directory to cmake, then choose your prefer build system.
+
+3. Use your build system to open the project, then build in it.
 
 ## features
 
--- cmake/toolchain-arm-none-eabi.cmake
+++ cmake/toolchain-arm-none-eabi.cmake
@@ -3,21 +3,18 @@ SET(CMAKE_SYSTEM_NAME Generic)
 #this one not so much
 SET(CMAKE_SYSTEM_VERSION 1)
 
-# specify the cross compiler
 IF(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-    SET(ARM_NONE_EABI_PATH C:/GNU_Tools_ARM_Embedded/6_2017_q2_update)
     SET(TOOLCHAIN_SUFFIX .exe)
-ELSE()
-    SET(ARM_NONE_EABI_PATH /usr)
 ENDIF()
 
-SET(CMAKE_C_COMPILER   ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-gcc${TOOLCHAIN_SUFFIX})
-SET(CMAKE_CXX_COMPILER ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-g++${TOOLCHAIN_SUFFIX})
-SET(OBJ_COPY ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-objcopy${TOOLCHAIN_SUFFIX})
-SET(OBJ_SIZE ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-size${TOOLCHAIN_SUFFIX})
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   arm-none-eabi-gcc${TOOLCHAIN_SUFFIX})
+SET(CMAKE_CXX_COMPILER arm-none-eabi-g++${TOOLCHAIN_SUFFIX})
+SET(OBJ_COPY arm-none-eabi-objcopy${TOOLCHAIN_SUFFIX})
+SET(OBJ_SIZE arm-none-eabi-size${TOOLCHAIN_SUFFIX})
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH ${ARM_NONE_EABI_PATH}/lib/gcc/arm-none-eabi)
+SET(CMAKE_FIND_ROOT_PATH ${ARM_NONE_EABI_HOME}/lib/gcc/arm-none-eabi)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
