d18338db66dca7202db36d00f5d8852c81e67b78
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-09-20 08:56:07 +0800

    add windows support.

--- cmake/toolchain-arm-none-eabi.cmake
+++ cmake/toolchain-arm-none-eabi.cmake
@@ -4,13 +4,20 @@ SET(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /usr/bin/arm-none-eabi-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/arm-none-eabi-g++)
-SET(OBJ_COPY /usr/bin/arm-none-eabi-objcopy)
-SET(OBJ_SIZE /usr/bin/arm-none-eabi-size)
+IF(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    SET(ARM_NONE_EABI_PATH C:/GNU_Tools_ARM_Embedded/6_2017_q2_update)
+    SET(TOOLCHAIN_SUFFIX .exe)
+ELSE()
+    SET(ARM_NONE_EABI_PATH /usr)
+ENDIF()
+
+SET(CMAKE_C_COMPILER   ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-gcc${TOOLCHAIN_SUFFIX})
+SET(CMAKE_CXX_COMPILER ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-g++${TOOLCHAIN_SUFFIX})
+SET(OBJ_COPY ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-objcopy${TOOLCHAIN_SUFFIX})
+SET(OBJ_SIZE ${ARM_NONE_EABI_PATH}/bin/arm-none-eabi-size${TOOLCHAIN_SUFFIX})
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /usr/lib/gcc/arm-none-eabi)
+SET(CMAKE_FIND_ROOT_PATH ${ARM_NONE_EABI_PATH}/lib/gcc/arm-none-eabi)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
